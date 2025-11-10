f0205b625893de8d78a253ac7fd68875a6e2da99
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-11-10 22:51:32 +0900

    Builder.Default was causing build problems so removed

--- src/main/java/com/hugbo/clock_in/dto/response/ShiftCompleteDTO.java
+++ src/main/java/com/hugbo/clock_in/dto/response/ShiftCompleteDTO.java
@@ -3,18 +3,14 @@ package com.hugbo.clock_in.dto.response;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class ShiftCompleteDTO {
     public ShiftDTO shift;
-    @Builder.Default
     public List<ShiftTaskDTO> shiftTasks = List.of();
-    @Builder.Default
     public List<ShiftBreakDTO> shiftBreaks = List.of();
-    @Builder.Default
     public List<EditRequestDTO> editRequests = List.of();
     // and so on
 }
