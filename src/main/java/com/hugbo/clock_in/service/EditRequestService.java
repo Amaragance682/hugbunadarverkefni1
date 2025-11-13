0f4c439709622020d60ad13902f4e76d9cf58968
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-11-13 11:49:21 +0900

    oops was using list instead of arraylist

--- src/main/java/com/hugbo/clock_in/service/EditRequestService.java
+++ src/main/java/com/hugbo/clock_in/service/EditRequestService.java
@@ -138,9 +138,9 @@ public class EditRequestService {
             .contract(contract)
             .startTs(requestedChanges.startTs)
             .endTs(requestedChanges.endTs)
+            .shiftTasks(new ArrayList<>())
+            .shiftBreaks(new ArrayList<>())
             .build();
-        newShift.shiftTasks = List.of();
-        newShift.shiftBreaks = List.of();
         for (ShiftTaskRequestDTO rcShiftTask : requestedChanges.shiftTasks) {
             Task task = taskRepository.findById(rcShiftTask.taskId).orElseThrow();
             ShiftTask newShiftTask = ShiftTask.builder()
